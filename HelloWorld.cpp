8b699fe923b7f0a5fd2c50881a46caebee5d6470
Tushar Sahu <100074664+Anicrate@users.noreply.github.com>
2022-04-29 22:29:57 +0530

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // cout << "Hello World";
+
+    string str;
+    cout << "Enter your Name" << endl;
+    cin >> str;
+    cout << "Hello " << str;
+
+    return 0;
+}
