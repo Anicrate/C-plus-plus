40355429bacdddc5b702286fb434eb80078333ba
Tushar Sahu <100074664+Anicrate@users.noreply.github.com>
2022-04-30 18:15:02 +0530

    Add files via upload

---
+++ Typecasting.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 7;
+    int y = 5;
+    float z;
+    z = x / y;
+    cout << z << endl;
+    z = (float)x / y;
+    cout << z;
+    return 0;
+}
